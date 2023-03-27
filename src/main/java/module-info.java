0c851139ddd5d484c7d5689a90858b077b4cf702
MATC10 <miguelangel.s309600@cesurformacion.com>
2023-03-27 22:14:04 +0200

    added graphical appearance with JavaFX

---
+++ src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module theSummoners {
+    requires javafx.fxml;
+    requires javafx.controls;
+
+    opens org.demofx.model;
+    opens org.demofx.controller;
+}
---
+++ src/main/java/org/thesummoners/model/Main.java
@@ -0,0 +1,23 @@
+package org.thesummoners.model;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/fxml/ventana.fxml")));
+
+
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
