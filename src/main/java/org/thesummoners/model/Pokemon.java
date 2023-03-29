25e339d1a5f42ccd6f21b426b7acd0b0b4b0f2d9
MATC10 <miguelangel.s309600@cesurformacion.com>
2023-03-29 11:48:48 +0200

    Objeto class finished, Pokemon class in process

--- src/main/java/org/thesummoners/model/Pokemon.java
+++ src/main/java/org/thesummoners/model/Pokemon.java
@@ -1,8 +1,11 @@
 package org.thesummoners.model;
 
+import java.util.Arrays;
+import java.util.Objects;
+
 public class Pokemon {
-    //id_pokemon
-    private int counterPokemon;
+    static int counterPokemon;
+    private int idPokemon;
     private int idPokedex;
     private String name;
     private String nickName;
@@ -10,6 +13,7 @@ public class Pokemon {
     private int level;
     private int fertility;
     private int attackPower;
+    private int specialAttack;
     private int defense;
     private int specialDefense;
     private int speed;
@@ -21,9 +25,37 @@ public class Pokemon {
     //METER AQUÍ ATRIBUTO MOVIMIENTOS?
     //AQUÍ PONER EN EN CONSTRUCTOR QUE SON 4
     private String[] learnedMovement;
+    private Objeto objeto;
+
+    //parametro: seleccionamos nombre viejo de movimiento,
+    // y nombre nuevo
+    public void assignNewMovement(String oldMove, Movement movement){
+        learnedMovement = new String[4];
+        for(int i = 0; i < learnedMovement.length; i++){
+            if(learnedMovement[i].equals(oldMove)){
+                learnedMovement[i] = newMove;
+            }
+        }
+        for(String a : learnedMovement){
+            if (a.equals(oldMove)){
+
+            }
+        }
+    }
 
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Pokemon pokemon = (Pokemon) o;
+        return idPokemon == pokemon.idPokemon && idPokedex == pokemon.idPokedex && hp == pokemon.hp && level == pokemon.level && fertility == pokemon.fertility && attackPower == pokemon.attackPower && specialAttack == pokemon.specialAttack && defense == pokemon.defense && specialDefense == pokemon.specialDefense && speed == pokemon.speed && stamina == pokemon.stamina && Objects.equals(name, pokemon.name) && Objects.equals(nickName, pokemon.nickName) && type1 == pokemon.type1 && type2 == pokemon.type2 && state == pokemon.state && sex == pokemon.sex && Arrays.equals(learnedMovement, pokemon.learnedMovement) && Objects.equals(objeto, pokemon.objeto);
+    }
 
-
-
+    @Override
+    public int hashCode() {
+        int result = Objects.hash(idPokemon, idPokedex, name, nickName, hp, level, fertility, attackPower, specialAttack, defense, specialDefense, speed, stamina, type1, type2, state, sex, objeto);
+        result = 31 * result + Arrays.hashCode(learnedMovement);
+        return result;
+    }
 }
