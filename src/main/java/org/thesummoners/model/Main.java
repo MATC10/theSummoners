a65ab3a2c6e98cc278dfdf25b1699fb7575538e9
MATC10 <miguelangel.s309600@cesurformacion.com>
2023-04-02 13:47:45 +0200

    Modified the Trainer constructor to test with that Class. Added Login controller for Login window. Added the Login controller for the Login window. Added links between Login, Registration and MainWindow. Added Registration and Login function. Functionally.

--- src/main/java/org/thesummoners/model/Main.java
+++ src/main/java/org/thesummoners/model/Main.java
@@ -11,8 +11,8 @@ import java.util.Objects;
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/fxml/DragPokemonIntoBox.fxml")));
-        Scene scene = new Scene(root, 600, 400);
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/fxml/Login.fxml")));
+        Scene scene = new Scene(root, 400, 440);
         stage.setTitle("Helloooo!");
         stage.setScene(scene);
         stage.show();
