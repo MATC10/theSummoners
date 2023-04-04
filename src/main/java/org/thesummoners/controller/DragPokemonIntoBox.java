d08caca5e5174d3a41111eb04458b09e06296644
MATC10 <miguelangel.s309600@cesurformacion.com>
2023-04-05 00:19:37 +0200

    Modified DragPokemonIntoBox and MainWindow drivers as well as fxml files

--- src/main/java/org/thesummoners/controller/DragPokemonIntoBox.java
+++ src/main/java/org/thesummoners/controller/DragPokemonIntoBox.java
@@ -1,14 +1,22 @@
 package org.thesummoners.controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
 import org.thesummoners.model.Objeto;
 import org.thesummoners.model.Pokemon;
 import org.thesummoners.model.Trainer;
 
+import java.io.IOException;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Objects;
 
 import static org.thesummoners.model.Trainer.getTrainer;
 
@@ -26,7 +34,13 @@ public class DragPokemonIntoBox {
     @FXML
     private Button btn6;
     @FXML
+    private Button btnBack;
+    @FXML
     private Label lbl1;
+    private Parent root;
+    private Scene scene;
+    private Stage stage;
+
 
     private Pokemon pokemon1 = new Pokemon("Bulbasaur", 1);
     private Pokemon pokemon2 = new Pokemon("Pikachu", 1);
@@ -88,6 +102,15 @@ public class DragPokemonIntoBox {
     public void pokemonIntoBox6() {
             getTrainer().dragPokemonIntoBox(5);
             btn6.setText("No Pokémon");
+    }
+    @FXML
+    private void toMainWindow(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/fxml/MainWindow.fxml")));
+        scene = new Scene(root, 600, 400);
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        stage.setTitle("TheSummoners");
+        stage.setScene(scene);
+        stage.show();
     }
 
 }
