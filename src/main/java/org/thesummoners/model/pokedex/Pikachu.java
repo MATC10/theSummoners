3c300b3bdae4a78cc2d0197baa4b3c7327369bdc
MATC10 <miguelangel.s309600@cesurformacion.com>
2023-04-14 14:51:15 +0200

    - Added imageBack attribute to Pokemon Class - Modified some controllers to adapt the images of the Pokémon - Added numberPokemonInTeam() method to the Trainer Class to know the number of Pokémon we have in the team. - Modified the dragPokemonIntoBox() method of the Trainer class to always have at least one Pokémon in the team. - Modified the DragPokemonIntoBox controller to adjust the changes made to the number of Pokémon in the pokemonTeam

--- src/main/java/org/thesummoners/model/pokedex/Pikachu.java
+++ src/main/java/org/thesummoners/model/pokedex/Pikachu.java
@@ -5,6 +5,7 @@ import org.thesummoners.model.Pokemon;
 public class Pikachu extends Pokemon {
     public Pikachu (String name, int level) {
         super(name, level);
-        setImage("doc/images/spritesback/3a-b__025__xy.gif");
+        this.setImage("doc/images/Pikachu.png");
+        this.setImageBack("doc/images/spritesback/3a-b__025__xy.gif");
     }
 }
