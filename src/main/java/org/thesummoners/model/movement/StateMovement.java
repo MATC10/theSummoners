caa9da3403b59974e406f663bcca99e8612756a4
MATC10 <miguelangel.s309600@cesurformacion.com>
2023-05-02 23:49:33 +0200

    stateCombat done

--- src/main/java/org/thesummoners/model/movement/StateMovement.java
+++ src/main/java/org/thesummoners/model/movement/StateMovement.java
@@ -1,6 +1,8 @@
 package org.thesummoners.model.movement;
 
+import org.thesummoners.model.pokemon.Pokemon;
 import org.thesummoners.model.pokemon.State;
+import org.thesummoners.model.trainer.Turns;
 
 import java.util.Random;
 
@@ -36,6 +38,18 @@ public class StateMovement extends Movement implements IStaminaCalculable {
     public void TurnsDurationcalculation(){
         Random random = new Random();
         this.numberOfTurnsDuration = random.nextInt(4)+1;
+    }
+
+//TODO METER ESTO EN FIGHT Y CALCULAR LO DE LOS TURNOS PARA EL TRAINER Y EL ENEMIGO SE METE DE MARAMETRO Pokemon1 o Pokemon2
+//TODO SEGÚN EL QUE SEA
+    public static void stateCombat(Pokemon pokemon, Movement movement) {
+        //COMPROBAMOS QUE EL MOVIMIENTO SEA DE ATAQUE Y CREAMOS LAS VARIABLES DE DAÑO
+        if (movement.getMovementType().equals("state")) {
+            //HACEMOS UN CASTING PARA OBTENER LOS MÉTODOS DE StateMovement
+            StateMovement stateMovement = (StateMovement) movement;
+            //CAMBIAMOS EL ESTADO DEL POKÉMON ENEMIGO AL ESTADO QUE APLICA EL MOVIMIENTO
+            pokemon.setState(stateMovement.getStateToApply());
+        }
     }
 
     @Override
--- src/main/java/org/thesummoners/model/pokemon/State.java
+++ src/main/java/org/thesummoners/model/pokemon/State.java
@@ -1,5 +1,38 @@
 package org.thesummoners.model.pokemon;
 
+import javafx.scene.control.Label;
+
 public enum State {
-    PARALYSED, BURNED, POISONED, ASLEEP, FROZEN, SLEEPY, DEBILITATED, ALIVE
+    PARALYSED, BURNED, POISONED, ASLEEP, FROZEN, DEBILITATED, ALIVE, RESTING;
+
+    public void applyState(Pokemon pokemon, Label lblTextFight) throws InterruptedException {
+        if(pokemon.getState() == DEBILITATED){
+            //TODO AQUÍ CUANDO ESTÉ VINCULADO CON LA BD TENEMOS
+            // QUE PONER LA VENTANA PARA QUE CAMBIE DE POKEMON
+        }
+        if(pokemon.getState() == ASLEEP){
+            lblTextFight.setText(pokemon.getDisplayName() + " se encuentra dormido");
+            Thread.sleep(1000);
+        }
+        if(pokemon.getState() == RESTING){
+
+        }
+        if(pokemon.getState() == FROZEN){
+
+        }
+        if(pokemon.getState() == BURNED){
+
+        }
+        if(pokemon.getState() == POISONED){
+
+        }
+        if(pokemon.getState() == PARALYSED){
+
+        }
+        if(pokemon.getState() == ALIVE){
+
+        }
+
+    }
 }
+
