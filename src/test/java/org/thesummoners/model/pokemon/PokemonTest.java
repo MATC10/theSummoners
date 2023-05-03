db043bb14cd800ada2f6a8f9d6601ad03e3daa82
badrlah9 <locopedro1507@gmail.com>
2023-05-03 13:42:41 +0200

    Junit test
    Hacer un test Con Junit a la clase Pokemon y Trainer con su Methodos.

---
+++ src/test/java/org/thesummoners/model/pokemon/PokemonTest.java
@@ -0,0 +1,356 @@
+package org.thesummoners.model.pokemon;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PokemonTest {
+
+    @org.junit.jupiter.api.Test
+    void getCounterPokemon() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setCounterPokemon() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getIdPokemon() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setIdPokemon() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getIdPokedex() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void train() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void equiparObjeto() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void desequiparObjeto() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getObjetoEquipado() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void adaptStatsToLevel() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void changeDisplayName() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testEquals() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testHashCode() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setItem() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void removeITEM() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getHealth() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setHealth() {
+    }
+
+    @Test
+    void testGetCounterPokemon() {
+    }
+
+    @Test
+    void testSetCounterPokemon() {
+    }
+
+    @Test
+    void testGetIdPokemon() {
+    }
+
+    @Test
+    void testSetIdPokemon() {
+    }
+
+    @Test
+    void testGetIdPokedex() {
+    }
+
+    @Test
+    void setIdPokedex() {
+    }
+
+    @Test
+    void getName() {
+    }
+
+    @Test
+    void setName() {
+    }
+
+    @Test
+    void getImage() {
+    }
+
+    @Test
+    void setImage() {
+    }
+
+    @Test
+    void getPokedex() {
+    }
+
+    @Test
+    void setPokedex() {
+    }
+
+    @Test
+    void getNickName() {
+    }
+
+    @Test
+    void setNickName() {
+    }
+
+    @Test
+    void getDisplayName() {
+    }
+
+    @Test
+    void setDisplayName() {
+    }
+
+    @Test
+    void setExperience() {
+    }
+
+    @Test
+    void getHp() {
+    }
+
+    @Test
+    void setHp() {
+    }
+
+    @Test
+    void getLevel() {
+    }
+
+    @Test
+    void setLevel() {
+    }
+
+    @Test
+    void getFertility() {
+    }
+
+    @Test
+    void setFertility() {
+    }
+
+    @Test
+    void getAttackPower() {
+    }
+
+    @Test
+    void setAttackPower() {
+    }
+
+    @Test
+    void getSpecialAttack() {
+    }
+
+    @Test
+    void setSpecialAttack() {
+    }
+
+    @Test
+    void getDefense() {
+    }
+
+    @Test
+    void setDefense() {
+    }
+
+    @Test
+    void getSpecialDefense() {
+    }
+
+    @Test
+    void setSpecialDefense() {
+    }
+
+    @Test
+    void getSpeed() {
+    }
+
+    @Test
+    void setSpeed() {
+    }
+
+    @Test
+    void getStamina() {
+    }
+
+    @Test
+    void setStamina() {
+    }
+
+    @Test
+    void getType1() {
+    }
+
+    @Test
+    void setType1() {
+    }
+
+    @Test
+    void getImageBack() {
+    }
+
+    @Test
+    void setImageBack() {
+    }
+
+    @Test
+    void getType2() {
+    }
+
+    @Test
+    void setType2() {
+    }
+
+    @Test
+    void getState() {
+    }
+
+    @Test
+    void setState() {
+    }
+
+    @Test
+    void getSex() {
+    }
+
+    @Test
+    void setSex() {
+    }
+
+    @Test
+    void getMovementLevel() {
+    }
+
+    @Test
+    void setMovementLevel() {
+    }
+
+    @Test
+    void getExperience() {
+    }
+
+    @Test
+    void getLearnedMovement() {
+    }
+
+    @Test
+    void learnedMovement() {
+    }
+
+    @Test
+    void setLearnedMovement() {
+    }
+
+    @Test
+    void getObjeto() {
+    }
+
+    @Test
+    void setObjeto() {
+    }
+
+    @Test
+    void assignMovement() {
+    }
+
+    @Test
+    void rest() {
+    }
+
+    @Test
+    void levelUp() {
+    }
+
+    @Test
+    void testTrain() {
+    }
+
+    @Test
+    void testEquiparObjeto() {
+    }
+
+    @Test
+    void testDesequiparObjeto() {
+    }
+
+    @Test
+    void testGetObjetoEquipado() {
+    }
+
+    @Test
+    void testAdaptStatsToLevel() {
+    }
+
+    @Test
+    void testChangeDisplayName() {
+    }
+
+    @Test
+    void testEquals1() {
+    }
+
+    @Test
+    void testHashCode1() {
+    }
+
+    @Test
+    void testSetItem() {
+    }
+
+    @Test
+    void testRemoveITEM() {
+    }
+
+    @Test
+    void testGetHealth() {
+    }
+
+    @Test
+    void testSetHealth() {
+    }
+
+    @Test
+    void getItem() {
+    }
+
+    @Test
+    void testToString() {
+    }
+}
---
+++ src/test/java/org/thesummoners/model/trainer/TrainerTest.java
@@ -0,0 +1,124 @@
+package org.thesummoners.model.trainer;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TrainerTest {
+
+    @Test
+    void getTrainer() {
+    }
+
+    @Test
+    void getName() {
+    }
+
+    @Test
+    void getPassword() {
+    }
+
+    @Test
+    void setName() {
+    }
+
+    @Test
+    void getPokemonTeam() {
+    }
+
+    @Test
+    void setPokemonTeam() {
+    }
+
+    @Test
+    void getPokedollar() {
+    }
+
+    @Test
+    void setPokedollar() {
+    }
+
+    @Test
+    void setPassword() {
+    }
+
+    @Test
+    void getPokeball() {
+    }
+
+    @Test
+    void setPokeball() {
+    }
+
+    @Test
+    void getPokemonPcBill() {
+    }
+
+    @Test
+    void setPokemonPcBill() {
+    }
+
+    @Test
+    void getBackPack() {
+    }
+
+    @Test
+    void setBackPack() {
+    }
+
+    @Test
+    void numberPokemonInTeam() {
+    }
+
+    @Test
+    void dragPokemonIntoBox() {
+    }
+
+    @Test
+    void dragPokemonIntoTeam() {
+    }
+
+    @Test
+    void pokemonBreeding() {
+    }
+
+    @Test
+    void fight() {
+    }
+
+    @Test
+    void checkPokemonTeamFull() {
+    }
+
+    @Test
+    void pokeballCount() {
+    }
+
+    @Test
+    void capture() {
+    }
+
+    @Test
+    void pokemonTeamArrayToList() {
+    }
+
+    @Test
+    void pokemonListToPokemonTeam() {
+    }
+
+    @Test
+    void objetoShop() {
+    }
+
+    @Test
+    void testEquals() {
+    }
+
+    @Test
+    void testHashCode() {
+    }
+
+    @Test
+    void manageObject() {
+    }
+}
