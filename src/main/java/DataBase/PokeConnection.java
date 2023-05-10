3cec9ec5ef24ecd9e644543d07ace80868677615
badrlah9 <locopedro1507@gmail.com>
2023-05-10 09:52:49 +0200

    PokeConnection Update

--- src/main/java/DataBase/PokeConnection.java
+++ src/main/java/DataBase/PokeConnection.java
@@ -9,34 +9,22 @@ import static javafx.application.Application.launch;
 public class PokeConnection {
 
     public static void main(String[] args) {
-
         String url = "jdbc:mysql://localhost:3306/pokemon";
-
         String usuario = "root";
-
         String password = "";
-
         String consulta = "SELECT * FROM pokedex";
 
-        try {
-
+        try{
             Class.forName("com.mysql.cj.jdbc.Driver");
-
             Connection connection = DriverManager.getConnection(url, usuario, password);
-
-
             System.out.println("Conectado");
-
-        } catch (SQLException e) {
-
-            System.out.println("Error");
-
-        } catch (ClassNotFoundException e) {
-
-            throw new RuntimeException(e);
-
         }
-
+        catch (SQLException e) {
+            System.out.println("Error");
+        }
+        catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
         launch();
     }
 }
