1dd30e1914781180aa73a4c25ff45a8ce022185b
badrlah9 <locopedro1507@gmail.com>
2023-05-10 09:50:59 +0200

    DataBase / PokeConnection
    Hecho una clase con una la  Connexion a My sql

---
+++ src/main/java/DataBase/PokeConnection.java
@@ -0,0 +1,42 @@
+package DataBase;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import static javafx.application.Application.launch;
+
+public class PokeConnection {
+
+    public static void main(String[] args) {
+
+        String url = "jdbc:mysql://localhost:3306/pokemon";
+
+        String usuario = "root";
+
+        String password = "";
+
+        String consulta = "SELECT * FROM pokedex";
+
+        try {
+
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            Connection connection = DriverManager.getConnection(url, usuario, password);
+
+
+            System.out.println("Conectado");
+
+        } catch (SQLException e) {
+
+            System.out.println("Error");
+
+        } catch (ClassNotFoundException e) {
+
+            throw new RuntimeException(e);
+
+        }
+
+        launch();
+    }
+}
--- src/main/java/org/thesummoners/model/Main.java
+++ src/main/java/org/thesummoners/model/Main.java
@@ -18,38 +18,4 @@ public class Main extends Application {
         stage.setScene(scene);
         stage.show();
     }
-
-    public static void main(String[] args) {
-
-        String url = "jdbc:mysql://localhost:3306/pokemon";
-
-        String usuario = "root";
-
-        String password = "";
-
-        String consulta = "SELECT * FROM pokedex";
-
-        try {
-
-            Class.forName("com.mysql.cj.jdbc.Driver");
-
-            Connection connection = DriverManager.getConnection(url, usuario, password);
-
-
-            System.out.println("Conectado");
-
-        } catch (SQLException e) {
-
-            System.out.println("Error");
-
-        } catch (ClassNotFoundException e) {
-
-            throw new RuntimeException(e);
-
-        }
-
-
-
-        launch();
-    }
 }
