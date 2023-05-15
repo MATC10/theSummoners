d1dee560b8078f776164f2a8092d74ad0bfc5128
badrlah9 <locopedro1507@gmail.com>
2023-05-15 10:31:46 +0200

    Logger Update

--- src/main/java/org/thesummoners/model/Logger.java
+++ src/main/java/org/thesummoners/model/Logger.java
@@ -19,6 +19,28 @@ public class Logger implements AutoCloseable{
             e.printStackTrace();
         }
     }
+    public void log(String message) {
+        LocalDateTime now = LocalDateTime.now();
+        String timestamp = now.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+        String logMessage = timestamp + " - " + message;
+
+        try {
+            writer.write(logMessage);
+            writer.newLine();
+            writer.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void close() {
+        try {
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
 
 }
