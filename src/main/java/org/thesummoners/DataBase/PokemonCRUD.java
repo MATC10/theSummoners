0442d3d6bb0d7072ede775b589ec1dc4afda5ce0
badrlah9 <locopedro1507@gmail.com>
2023-05-17 18:55:00 +0200

    Update Clase Login

--- src/main/java/org/thesummoners/DataBase/PokemonCRUD.java
+++ src/main/java/org/thesummoners/DataBase/PokemonCRUD.java
@@ -208,15 +208,6 @@ public class PokemonCRUD {
 
         return listaPokemon;
     }
-
-
-
-
-
-
-
-
-
     public static void updatePokemon() {
 
 
@@ -237,70 +228,4 @@ public class PokemonCRUD {
         }
         return true;
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-    public static LinkedList<Pokemon> readPokemon() {
-        String query = "select * from pokedex";
-
-        PreparedStatement preparedStatement = null;
-        LinkedList<Pokemon> listaPokemon = new LinkedList<>();
-        try {
-            preparedStatement = MySQLConnection.getConnection().prepareStatement(query);
-            //preparedStatement.setInt(1, id);
-            ResultSet resultSet = preparedStatement.executeQuery();
-
-            while (resultSet.next()) {
-                String name = resultSet.getString("name");
-                int idPokedex = resultSet.getInt("ID_Pokedex");
-                String image = resultSet.getString("Image");
-                String imageBack = resultSet.getString("Image_Back");
-                int hp = resultSet.getInt("HP");
-                int level = resultSet.getInt("Level");
-                int attackPower = resultSet.getInt("AttackPower");
-                int specialAttack = resultSet.getInt("SpecialAttack");
-                int defense = resultSet.getInt("Defense");
-                int specialDefense = resultSet.getInt("SpecialDefense");
-                int speed = resultSet.getInt("Speed");
-                int stamina = resultSet.getInt("Stamina");
-                String type1 = resultSet.getString("Type1");
-                String type2 = resultSet.getString("Type2");
-                String state = resultSet.getString("State");
-                String sex = resultSet.getString("Sex");
-                int experience = resultSet.getInt("experience");
-
-                listaPokemon.add(new Pokemon(name, idPokedex, image, imageBack, hp, level, attackPower,
-                        specialAttack,defense,specialDefense,speed,stamina,Type.valueOf(type1), Type.valueOf(type2),
-                        State.valueOf(state), Sex.valueOf(sex), experience));
-            }
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-
-        return listaPokemon;
-    }
-
-    public static void updatePokemon() {
-        System.out.println(Trainer.getTrainer().getPokemon1());
-    }
-
-    public static boolean deletePokemon() {
-        return true;
-    }       */
-
-
 }
--- src/main/java/org/thesummoners/controller/Login.java
+++ src/main/java/org/thesummoners/controller/Login.java
@@ -7,11 +7,17 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import org.thesummoners.DataBase.PokemonCRUD;
+import org.thesummoners.model.pokemon.Pokedex;
+import org.thesummoners.model.pokemon.Pokemon;
 import org.thesummoners.model.trainer.Trainer;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.LinkedList;
 import java.util.Objects;
 
 public class Login {
@@ -56,4 +62,23 @@ public class Login {
         stage.setScene(scene);
         stage.show();
     }
+    public void initialize() throws CloneNotSupportedException {
+//TRAIGO LOS POKEMON DE LA POKEDEX
+        LinkedList<Pokemon> miLista =  (LinkedList<Pokemon>) PokemonCRUD.readPokemon();
+        for( Pokemon p : miLista){
+            Pokedex.getPokedex().add(p);
+        }
+
+
+        //TRAIGO LOS POKEMON DEL EQUIPO, SI LOS HUBIERA
+        LinkedList<Pokemon> miListaTeam =  (LinkedList<Pokemon>) PokemonCRUD.readPokemonTeam();
+        for(int i = 0; i < Trainer.getTrainer().getPokemonTeam().length && i < miListaTeam.size(); i++)
+            Trainer.getTrainer().getPokemonTeam()[i] = miListaTeam.get(i);
+
+
+        //TRAIGO LOS POKEMON DEL PC, SI LOS HUBIERA
+        Trainer.getTrainer().getPokemonPcBill().clear();
+        LinkedList<Pokemon> miListaPc =  (LinkedList<Pokemon>) PokemonCRUD.readPokemonPcBill();
+        Trainer.getTrainer().getPokemonPcBill().addAll(miListaPc);
+        
 }
